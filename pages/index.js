81bab5d87918ae7af35d63a8982330c00381d59f
Fuad1877 <107192490+Fuad1877@users.noreply.github.com>
2022-09-12 16:31:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,26 @@ export default function Home() {
     </div>
   )
 }
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Bam zwana <code>test</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
